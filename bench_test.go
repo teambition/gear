6b9ec3ec4f16083d86883d8efb05d10143cda71b
zensh <admin@zensh.com>
2016-12-28 10:59:47 +0800

    update bench

--- bench_test.go
+++ bench_test.go
@@ -1,34 +1,38 @@
 package gear_test
 
 import (
-	"io/ioutil"
 	"net/http"
 	"testing"
 
 	"github.com/teambition/gear"
 )
 
+// go test -bench=. -run BenchmarkGearAppHello
+// 10000	    353521 ns/op	   17469 B/op	     131 allocs/op
+//
 func BenchmarkGearAppHello(b *testing.B) {
 	app := gear.New()
+	for i := 0; i < 100; i++ {
+		app.Use(func(ctx *gear.Context) error {
+			return nil
+		})
+	}
 	app.Use(func(ctx *gear.Context) error {
 		return ctx.End(200, []byte("<h1>Hello!</h1>"))
 	})
 	srv := app.Start()
 	defer srv.Close()
 
-	url := "http://" + srv.Addr().String()
-	res, _ := http.Get(url)
-	ioutil.ReadAll(res.Body)
-	res.Body.Close()
-
+	url := "http://" + srv.Addr().String() + "/?foo[bar]=baz"
+	b.N = 10000
+	b.ReportAllocs()
 	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		res, err := http.Get(url)
 		if err != nil {
 			panic(err)
 		}
-		ioutil.ReadAll(res.Body)
 		res.Body.Close()
 	}
-	// 2016-10-27: 20000	     79827 ns/op
 }
